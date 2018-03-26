0a70f6c5034f0970ec486eefccd72ca7db2bccc1
seapage <contact@seapage.pl>
2018-03-26 21:05:59 +0200

    fixed service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,39 +1,34 @@
+self.addEventListener('fetch', function(event) {
+    const port = 8000;
 
-const port = 8000;
+    return event.respondWith(
+        caches.open('fileStore').then((cacheOpend)=>{
+            let linkToOpen = event.request;
+    if(event.request.url.indexOf("restaurant.html") != -1){
+        linkToOpen = `http://localhost:${port}/restaurant.html`;
+    }
+    return cacheOpend.match(linkToOpen).then((response)=>{
+
+            return response || fetch(linkToOpen).then(function(response) {
+                cacheOpend.put(linkToOpen, response.clone());
+                return response;
+            });
+
+})
+
+
+
+}))})
+
 
 self.addEventListener('install', function(event) {
     caches.open('fileStore').then((cacheOpend) => {
         return cacheOpend.addAll([
             '/restaurant.html',
+            '/js/restaurant_info.js',
             '/'
-        ])}
+        ])})
+});
 
-            self.addEventListener('fetch', function(event) {
-
-
-                return event.respondWith(
-                    caches.open('fileStore').then((cacheOpend)=>{
-                        let linkToOpen = event.request;
-                if(event.request.url.indexOf("restaurant.html") != -1){
-                    linkToOpen = `http://localhost:${port}/restaurant.html`;
-                }
-                return cacheOpend.match(linkToOpen).then((response)=>{
-
-                        return response || fetch(linkToOpen).then(function(response) {
-                            cacheOpend.put(linkToOpen, response.clone());
-                            return response;
-                        });
-
-            })
-
-
-            })
-
-                );
-
-            });
-
-        )
-    })
 
 
